ce1cb3f6a532328af4ab3b22d245afb519737b62
Dan Lloyd <me@dglloyd.net>
2017-09-03 00:48:41 -0400

    Make Starr happy

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ int redPin = 5;
 int greenPin = 6;
 int bluePin = 3;
 
-int end[3] = { 190, 0, 240 };
+int end[3] = { 220, 0, 230 };
 int start[3] = { 74, 1, 30 };
 int current[3] = { 0, 0, 0 };
 
@@ -14,19 +14,15 @@ void setup()
  pinMode(redPin, OUTPUT);
  pinMode(greenPin, OUTPUT);
  pinMode(bluePin, OUTPUT);
-
- analogWrite(redPin, 182);
- analogWrite(greenPin, 153);
- analogWrite(bluePin, 255);
-
+Serial.begin(9600);
 }
 
 void loop()
 {
-    for (int i = 0; i <= 50; i++) {
-    current[0] = ((start[0] * (50 - i)) + (end[0] * i)) / 50;
-    current[1] = ((start[1] * (50 - i)) + (end[1] * i)) / 50;
-    current[2] = ((start[2] * (50 - i)) + (end[2] * i)) / 50;
+    for (int i = 0; i <= 100; i++) {
+    current[0] = ((start[0] * (100 - i)) + (end[0] * i)) / 100;
+    current[1] = ((start[1] * (100 - i)) + (end[1] * i)) / 100;
+    current[2] = ((start[2] * (100 - i)) + (end[2] * i)) / 100;
 
     analogWrite(redPin, current[0]);
     analogWrite(greenPin, current[1]);
@@ -34,16 +30,19 @@ void loop()
     char buffer[256];
     sprintf(buffer,"%d,%d,%d\n",current[0], current[1], current[2]);
     Serial.print(buffer);
-    delay(30);
+    delay(60);
     }
-    for (int i = 0; i <= 50; i++) {
-    current[0] = ((end[0] * (50 - i)) + (start[0] * i)) / 50;
-    current[1] = ((end[1] * (50 - i)) + (start[1] * i)) / 50;
-    current[2] = ((end[2] * (50 - i)) + (start[2] * i)) / 50;
+    for (int i = 0; i <= 100; i++) {
+    current[0] = ((end[0] * (100 - i)) + (start[0] * i)) / 100;
+    current[1] = ((end[1] * (100 - i)) + (start[1] * i)) / 100;
+    current[2] = ((end[2] * (100 - i)) + (start[2] * i)) / 100;
 
     analogWrite(redPin, current[0]);
     analogWrite(greenPin, current[1]);
     analogWrite(bluePin, current[2]);
-    delay(30);
+    char buffer[256];
+    sprintf(buffer,"%d,%d,%d\n",current[0], current[1], current[2]);
+    Serial.print(buffer);
+    delay(60);
     }
 }
